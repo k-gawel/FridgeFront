669e38fc572d7e9303703da52bd106217001eed9
Kamil <kamil.gawel.aplikacje@gmail.com>
2019-06-23 15:28:12 +0200

    Simplified implementations.

--- src/app/_models/response/KeyName.ts
+++ src/app/_models/response/KeyName.ts
@@ -3,14 +3,11 @@ import {Entity, EntityList} from './Entity';
 export class KeyName extends Entity {
     public name: string;
 
-    constructor(id?: any, name?: any) {
-        super();
+    constructor(json?: JSON) {
+        super(json);
 
-        if(id != undefined && name != undefined) {
-          this.id = id;
-          this.name = name;
-        }
-
+        if(json != undefined)
+          this.name = json['name'];
     }
 
     static clone(keyName: KeyName): KeyName {
@@ -19,120 +16,54 @@ export class KeyName extends Entity {
       result.name = keyName.name;
       return result;
     }
-
-    public fromJSON(json: JSON): KeyName {
-      this.id = json['id'];
-      this.name = json['name'];
-      return this;
-    }
-
-    public static fromJSON(json: JSON): KeyName {
-
-        const result: KeyName = new KeyName();
-
-        if(json['id'] != null && json['name'] != null) {
-          result.id = json['id'];
-          result.name = json['name'];
-        } else {
-
-        }
-
-        return result;
-
-    }
-
-
 }
 
 export class KeyNameList extends EntityList {
 
-
-    list: KeyName[];
-
+    list: KeyName[] = [];
 
     constructor(json?: JSON | JSON[]) {
         super();
-        this.list = [];
 
-        if(json != undefined) {
-          if (json instanceof Array)
-            this.fromJSONArray(json);
-          else
+        if(json == undefined)
+          return;
+        else if (json instanceof Array)
+          this.fromJSONArray(json);
+        else
             this.fromJSONObject(json);
-        }
-
-
     }
-
-    public static fromJSON(json: JSON[]): KeyNameList {
-
-    let result = new KeyNameList();
-
-    json.forEach( (e: JSON) => {
-      result.push(KeyName.fromJSON(e));
-    } );
-
-    return result;
-
-  }
-
 
     private fromJSONObject(json: JSON) {
       let keys = Object.keys(json);
-
-      for(let key of keys) {
-        let keyName = new KeyName(key, json[key]);
-        this.list.push(keyName);
-      }
+      this.list = keys.map(k => {
+        let result = new KeyName();
+        result.id = <number> k;
+        result.name = json[k];
+        return result;
+      });
     }
-
 
     private fromJSONArray(jsonArr: JSON[]) {
-
-      for(let json of jsonArr) {
-        let keyName = new KeyName();
-        keyName.fromJSON(json);
-        this.list.push(keyName);
-      }
-
+      this.list = jsonArr.map(j => new KeyName(j));
     }
-
 
     public getById(id: number): KeyName {
       return <KeyName> super.getById(id);
     }
 
-
     public getByIds(ids: number[]): KeyNameList {
       return <KeyNameList> super.getByIds(ids);
     }
 
-
-    public getAll(): KeyName[] {
+    public toArray(): KeyName[] {
         return this.list;
     }
 
-
     public searchByName(text: string): KeyNameList {
         const result = new KeyNameList();
-
-        this.list.forEach( (element: KeyName) => {
-            if (element.name.indexOf(text) !== -1) {
-                result.push(element);
-            }
-        });
-
+        result.list = this.list
+          .filter(kn => kn.name.toUpperCase().includes(text.toUpperCase()));
         return result;
     }
-
-
-    public containsKey(key: number): boolean {
-        for (let i = 0; i < this.list.length; i++) {
-            if (this.list[i].id == key) { return true; }
-        }
-        return false;
-    }
-
-
 
 }
