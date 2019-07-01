937996b9fe9846b6f2dbcf91aa1ee0b3f800addb
Kamil <kamil.gawel.aplikacje@gmail.com>
2019-07-01 16:16:32 +0200

    Api updated.

--- src/app/_service/api/place/place-api.service.ts
+++ src/app/_service/api/place/place-api.service.ts
@@ -1,20 +1,20 @@
 import {Injectable} from '@angular/core';
 import {ApiService} from '../api/api.service';
 import {HttpParams} from '@angular/common/http';
-import {PlaceForm} from '../../../_models/response/PlaceForm';
+import {PlaceForm} from '../../../_models/request/PlaceForm';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlaceApiService {
 
-  private url = this.api.url + "place/";
+  private url = this.api.url + "places";
 
   constructor(private api: ApiService) { }
 
 
   public newPlace(form: PlaceForm) {
-    let url = this.url + "new";
+    let url = this.url;
     let body = form;
     let header = this.api.getHeaderWithToken();
 
@@ -23,44 +23,34 @@ export class PlaceApiService {
 
 
   public get(placeIds: number[] | number) {
-    let url = this.url + "get";
-    let params = new HttpParams();
-    params = params.append("ids", ApiService.numbersArrayToString(placeIds));
+    let url = this.url + "/" + ApiService.numbersArrayToString(placeIds);
     let header = this.api.getHeaderWithToken();
 
-    return this.api.get(url, header, params);
+    return this.api.get(url, header, null);
   }
 
+
   public addUser(placeId: number, userId: number) {
-    let url = this.url + "addUser";
-    let params = new HttpParams();
-    params = params.append("placeId", placeId.toString());
-    params = params.append("userId", userId.toString());
+    let url = this.url + "/" + placeId + "/accounts/" + userId;
     let header = this.api.getHeaderWithToken();
 
-    return this.api.put<boolean>(url, params, null, header);
+    return this.api.post(url, null, header);
   }
 
 
   public changeAdmin(placeId: number, newAdminId: number) {
-    let url = this.url + "change_admin";
-    let params = new HttpParams();
-    params = params.append("place_id", placeId.toString());
-    params = params.append("new_admin_id", newAdminId.toString());
+    let url = this.url +"/" + placeId + "/admin/" + newAdminId;
     let header = this.api.getHeaderWithToken();
 
-    return this.api.put<boolean>(url, params, null, header);
+    return this.api.put(url, null, null, header);
   }
 
 
   public removeUser(placeId: number, userId: number) {
-    let url = this.url + "removeUser";
-    let params = new HttpParams();
-    params = params.append("placeId", placeId.toString());
-    params = params.append("userId", userId.toString());
+    let url = this.url + "/" + placeId + "/accounts/" + userId;
     let header = this.api.getHeaderWithToken();
 
-    return this.api.delete<boolean>(url, header, params);
+    return this.api.delete(url, header, null);
   }
 
 
