65a2deb6af4d64100cf2903bb88e4b47b26d1ce3
Kamil <kamil.gawel.aplikacje@gmail.com>
2019-07-01 16:46:05 +0200

    Simple refactor.

--- src/app/_component/guest/login/login.component.ts
+++ src/app/_component/guest/login/login.component.ts
@@ -1,8 +1,8 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {LoginForm} from '../../../_models/response/LoginForm';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {LoginForm} from '../../../_models/request/LoginForm';
 import {AuthService} from '../../../_service/auth/auth/auth.service';
 import {RoleContent} from '../../../_models/util/Content';
-import {AccountDatas} from '../../../_models/request/AccountDatas';
+import {AccountDatas} from '../../../_models/response/AccountDatas';
 import {ErrorMessage} from '../../../_models/util/ErrorMessage';
 
 @Component({
@@ -10,7 +10,7 @@ import {ErrorMessage} from '../../../_models/util/ErrorMessage';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
 
   @Output() account = new EventEmitter<AccountDatas>();
 
@@ -18,24 +18,20 @@ export class LoginComponent implements OnInit {
 
   constructor(private authService: AuthService) { }
 
-  ngOnInit() {
-  }
-
 
   submit() {
-    if(this.loginForm.validate())
-      this.authService.login(this.loginForm)
-        .then((result: AccountDatas) => {
-          if(result != null)
-            this.account.emit(result);
-          else
-            throw new ErrorMessage("loginaccount.nullable");
-        })
-        .catch((e: ErrorMessage) => {
-          this.loginForm.errors = e;
-        });
-  }
+    if(!this.loginForm.validate())
+      return;
 
+    this.authService.login(this.loginForm)
+      .then((result: AccountDatas) => {
+        if(result != null)
+          this.account.emit(result);
+        else
+          throw new ErrorMessage("loginaccount.nullable");
+      })
+      .catch((e: Error) => this.loginForm.errors = new ErrorMessage(e.message) );
+  }
 
   switchToRegister() {
     this.authService.$roleContent.next(RoleContent.GUEST_REGISTER);
